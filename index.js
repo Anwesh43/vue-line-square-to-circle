6a97e68adecf7abf320cfd923f4222ca62bc797c
Anwesh43 <anweshthecool0@gmail.com>
2019-10-29 16:18:16 +0530

    adding State class

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ class ScaleUtil {
         return scale + dir * scGap
     }
 }
+
+class State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale = ScaleUtil.update(scale, dir)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
